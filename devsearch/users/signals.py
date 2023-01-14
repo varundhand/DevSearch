8a652234115ef175bcc0c340b28552d61a98118b
varun dhand <varun@email.com>
2023-01-15 03:00:38 +0530

    login and regitser

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User # the default django admin users
 from .models import Profile
 
 # @receiver(post_save,sender=Profile) this is second method of using django signals 
-def createProfile(sender, instance, created, **kwargs):
+def createProfile(sender, instance, created, **kwargs):  #whenever a user is created a profile will be created
   if created: #if its the first instance of the user
-    user = instance
+    user = instance # instance is the sender i.e. 'User' 
     profile = Profile.objects.create(
       user= user,
       username = user.username,
@@ -18,7 +18,7 @@ def deleteUser(sender,instance, **kwargs):
   user = instance.user
   user.delete()
   
-  
 #connecting a reciever to a sender(reciever is the function 'profileUpdated' and sender is the model which triggers it i.e. 'Profile' )
 post_save.connect(createProfile, sender=User) 
 post_delete.connect(deleteUser, sender=Profile)
+  
