b4894428c65e0222bd60dbed1370ffee8aaf14be
varun dhand <varun@email.com>
2023-01-16 15:41:05 +0530

    added account url for editing the user profile

--- devsearch/staticfiles/uikit/app.js
+++ devsearch/staticfiles/uikit/app.js
@@ -4,11 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
  
  
-let alertWrapper = document.querySelector('.alert')
-let alertClose = document.querySelector('.alert__close')
- 
-if (alertWrapper) {
-  alertClose.addEventListener('click', () =>
-    alertWrapper.style.display = 'none'
-  )
-}
+let alertWrapper = document.querySelectorAll('.alert'); 
+let alertClose = document.querySelectorAll('.alert__close');
+  
+if(alertWrapper){
+    for (let i = 0; i < alertClose.length; i++) {
+     alertClose[i].addEventListener('click', ()=>{
+       alertWrapper[i].style.display = 'none'});
+       }
+  } 
